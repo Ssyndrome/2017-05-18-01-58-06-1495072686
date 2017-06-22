2ff5fbe48f598a5c1dbd931eff8614e908c78fc1
Ssyndrome <915448715@qq.com>
2017-06-23 02:52:33 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,6 @@
-module.exports = function main() {
-  // Write your cade here
+var sum = 0;
+module.exports = function main(str) {
+  for (var i = 0; i <str.length; i++){
+    sum += parseInt(str[i]);
+  }  
 };
